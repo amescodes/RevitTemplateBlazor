ce2b491229dfaa78e9bfea0fecd230a8f422659c
ames <56943426+amescodes@users.noreply.github.com>
2022-10-29 00:33:06 -0500

    Add project files.

---
+++ RevitTemplateWeb/ExtApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+
+namespace RevitTemplateWeb
+{
+    public class ExtApp : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
